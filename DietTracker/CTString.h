7c510e940e107239e8d7bfcddb444e8c34a0ddfd
cgtrout <cgtrout@hotmail.com>
2014-04-06 04:12:00 -0600

    CTString::split - fixed to pass testing

--- DietTracker/CTString.h
+++ DietTracker/CTString.h
@@ -13,17 +13,23 @@ vector<string> split(string &instring, string delim = " ")
 	
 	size_t pos = 0;
 	size_t lastpos = 0;
+	size_t length = 0;
 	while(pos != string::npos) {
 		pos = instring.find_first_of(delim, pos);
-		char tempDelim = instring[pos];
-		while(instring[pos++] == tempDelim);
-		
+				
 		if(pos != string::npos) {
-			vec.push_back(instring.substr(lastpos, pos - lastpos));
+			char tempDelim = instring[pos++];
+			length = pos - lastpos - 1;
+			while(instring[pos] == tempDelim) {
+				pos++;
+			}
+			vec.push_back(instring.substr(lastpos, length));
+		} else {
+			vec.push_back(instring.substr(lastpos, instring.length() - lastpos));
 		}
 		lastpos = pos;
 	} 
-	return split;
+	return vec;
 }
 
 #endif //CTSTRING
