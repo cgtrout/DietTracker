7064a0146b2f9e48fee7b8bf8c0b576f55b16ced
cgtrout <cgtrout@hotmail.com>
2014-04-06 01:21:39 -0600

    Test cin/cout

--- DietTracker/DietTracker.cpp
+++ DietTracker/DietTracker.cpp
@@ -1,8 +1,20 @@
 // DietTracker.cpp : Defines the entry point for the console application.
 //
+#include <iostream>
+#include <string>
 
+using namespace std;
 int main(int argc, char* argv[])
 {
+	string templine;
+	cout << "DietTracker > ";
+	cin >> templine;
+	cout << "Executing: " << templine << endl;
+
+	cout << "Hit enter to close";
+	
+	cin >> templine;
 	return 0;
 }
+
 
