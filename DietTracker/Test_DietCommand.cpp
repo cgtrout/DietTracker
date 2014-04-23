da59961540587dd7a52749398f0567a679d3a437
cgtrout <cgtrout@hotmail.com>
2014-04-23 04:07:02 -0600

    minor formatting

--- DietTracker/Test_DietCommand.cpp
+++ DietTracker/Test_DietCommand.cpp
@@ -12,75 +12,78 @@ extern Tester tester;
 using namespace std;
 void Test_DietCommand::addTests() {
      suiteDietCommand.AddTest("Add Diet Command", []()-> bool {
-          DietCommands dc;
-          dc.AddDietCommand("Test 1");
+         DietCommands dc;
+         dc.AddDietCommand( "Test 1" );
 
-          return dc.commands.size() == 1;
+         return dc.commands.size() == 1;
+     });
+     suiteDietCommand.AddTest("Test Diet Command Lambdas", []()-> bool {
+         return false;
      });
      suiteDietCommand.AddTest("Test CommandParamQuantity set", []()->bool {
-          DietCommands dc;
+         DietCommands dc;
 
-          dc.AddDietCommand("Test 2");
+         dc.AddDietCommand( "Test 2" );
 
-          DietCommand &command = dc.commands["Test 2"];
-          command.AddParam(make_unique<CommandParamQuantity>());
-          command.params[0]->SetValue("14.0s");
-          auto value = std::stof(command.params[0]->GetValue());
+         DietCommand &command = dc.commands[ "Test 2" ];
+         command.AddParam( make_unique<CommandParamQuantity>() );
+         command.params[ 0 ]->SetValue( "14.0s" );
+         auto value = std::stof( command.params[ 0 ]->GetValue() );
 
-          return value == 14.0;
+         return value == 14.0;
      });
      suiteDietCommand.AddTest("Test Quantity: invalid type ", []()->bool {
-          bool inException = false;
+         bool inException = false;
 
-          CommandParamQuantity q;
-          try {
-               q.SetValue("14.0a");
-          } catch(invalid_argument&) {
-               inException = true;
-          }
-          return inException;
-     });
-     suiteDietCommand.AddTest("Test Quantity: invalid float value ", []()->bool {
-          bool inException = false;
+         CommandParamQuantity q;
+         try {
+             q.SetValue( "14.0a" );
+         } catch( invalid_argument& ) {
+             inException = true;
+         }
+         return inException;
+     } );
+     suiteDietCommand.AddTest( "Test Quantity: invalid float value ", []()->bool {
+         bool inException = false;
 
-          CommandParamQuantity q;
-          try {
-               q.SetValue("a3434b");
-          } catch(invalid_argument&) {
-               inException = true;
-          }
-          return inException;
-     });
-     suiteDietCommand.AddTest("Test CommandParamQuantity Types", []()->bool {
-          CommandParamQuantity q;
-          q.SetValue("12.0");
-          q.SetType('g');
-          bool exceptionCaught = false;
-          try {
-               q.SetType('d');
-          } catch(invalid_argument&) {
-               exceptionCaught = true;
-          }
-          return exceptionCaught;
-     });
-     suiteDietCommand.AddTest("Test CommandParamDate", []()->bool {
-          auto testInput = "2002-01-25";
-          CommandParamDate param(testInput);
-          auto out = param.GetValue();
-          return out == testInput;
-     });
-     suiteDietCommand.AddTest("Test CommandParamString", []()->bool {
-          auto testInput = "Test1";
-          CommandParamString test(testInput);
-          
-          return testInput == test.GetValue();
-     });
-     suiteDietCommand.AddTest("Test CommandParamTime set/get", []()->bool {
-          auto testInput = "23:59:59";
-          CommandParamTime test(testInput);
-          
-          auto out = test.GetValue();
-          return testInput == out;
-     });
-     tester.AddSuite(suiteDietCommand);
+         CommandParamQuantity q;
+         try {
+             q.SetValue( "a3434b" );
+         } catch( invalid_argument& ) {
+             inException = true;
+         }
+         return inException;
+     } );
+     suiteDietCommand.AddTest( "Test CommandParamQuantity Types", []()->bool {
+         CommandParamQuantity q;
+         q.SetValue( "12.0" );
+         q.SetType( 'g' );
+         bool exceptionCaught = false;
+         try {
+             q.SetType( 'd' );
+         } catch( invalid_argument& ) {
+             exceptionCaught = true;
+         }
+         return exceptionCaught;
+     } );
+     suiteDietCommand.AddTest( "Test CommandParamDate", []()->bool {
+         auto testInput = "2002-01-25";
+         CommandParamDate param( testInput );
+         auto out = param.GetValue();
+         return out == testInput;
+     } );
+     suiteDietCommand.AddTest( "Test CommandParamString", []()->bool {
+         auto testInput = "Test1";
+         CommandParamString test( testInput );
+
+         return testInput == test.GetValue();
+     } );
+     suiteDietCommand.AddTest( "Test CommandParamTime set/get", []()->bool {
+         auto testInput = "23:59:59";
+         CommandParamTime test( testInput );
+
+         auto out = test.GetValue();
+         return testInput == out;
+     } );
+     tester.AddSuite( suiteDietCommand );
 }
