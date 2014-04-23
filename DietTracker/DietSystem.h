a29e42baa96d25866db319ec2bfb78b52bd9e9c6
cgtrout <cgtrout@hotmail.com>
2014-04-23 04:52:06 -0600

    Initial design of DietSystem class

--- DietTracker/DietSystem.h
+++ DietTracker/DietSystem.h
@@ -1,4 +1,19 @@
 #ifndef DIETSYSTEM_H
 #define DIETSYSTEM_H
 
+class DietSystem {
+    //DietCommands
+    //DailyLog
+
+    //--COMMAND FUNCTIONS--
+    //EAT
+    //DEFINE
+    //DELETE
+    //etc...
+
+    //BindFunctions
+    // binds all functions to dietCommands
+    void BindFunctions();
+};
+
 #endif
