475a0381197a01873bdac3fd09e7d74670163f78
cgtrout <cgtrout@hotmail.com>
2014-04-24 01:35:38 -0600

    Added test files for DietSystem

---
+++ DietTracker/Test_DietSystem.cpp
@@ -0,0 +1,12 @@
+#include "Test_DietSystem.h"
+
+TestSuite suiteDietSystem("Diet System");
+extern Tester tester;
+
+using namespace std;
+void Test_DietSystem::addTests()
+{
+    //TODO Add tests here
+
+    tester.AddSuite( suiteDietSystem );
+}
---
+++ DietTracker/Test_DietSystem.h
@@ -0,0 +1,9 @@
+#ifndef TEST_DIETSYSTEM_H
+#define TEST_DIETSYSTEM_H
+#include "Test.h"
+
+namespace Test_DietSystem
+{
+	void addTests();
+}
+#endif
