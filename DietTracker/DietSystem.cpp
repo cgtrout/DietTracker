b0deaa01bc605cbcffb27efb694a557ef1bffe8f
cgtrout <cgtrout@hotmail.com>
2014-04-26 06:54:08 -0600

    Param loading is now done in actual command function handler
    - this is to allow commands that handle different param types (such as eat)

--- DietTracker/DietSystem.cpp
+++ DietTracker/DietSystem.cpp
@@ -13,8 +13,8 @@ void DietSystem::ExecuteLine( const std::string &line )
     if( line.length() == 0 ) {
         throw invalid_argument( "Invalid command." );
     }
-    vector<string> lines = parseCommandLine( line );
-    string commandName = lines[0];
+    param_tokens = parseCommandLine( line );
+    string commandName = param_tokens[0];
     
     //convert commandName to lowercase
     //TODO pull this to seperate function?
@@ -24,37 +24,12 @@ void DietSystem::ExecuteLine( const std::string &line )
     if( !dietCommands.DoesCommandExist( commandName ) ) {
         throw invalid_argument( "Command does not exist" );
     }
-    
-    auto &thiscommand = dietCommands.commands[ commandName ];
 
-    //ensure valid number of arguments 
-    int defaultParam = thiscommand.hasDefaultParam;
-    if( (lines.size() - 1) != (thiscommand.params.size() - defaultParam) ) {
-        throw invalid_argument( "Incorrect number of params" );
-    }
-     
-    auto lineIter = lines.begin();
-    lineIter++;
-    
-    //set parameters
-    for( auto parami = 0u; parami < thiscommand.params.size() - defaultParam; ++parami ) {
-        auto &p = thiscommand.params[ parami ];
-        try {
-            p->SetValue( *lineIter );
-        } catch( invalid_argument & ) {
-            cout << commandName << ": Invalid parameter ( " << p->name << ")" << endl;
-            throw invalid_argument( "Invalid parameter" );
-        }
-        ++lineIter;
-    }
-
-    //if last parm is a default, set it
-    if( thiscommand.hasDefaultParam ) {
-        thiscommand.GetLastParam()->SetDefault();
-    }
+    InitializeCommand( commandName );
+    CheckParamCount( );
 
     //run function
-    thiscommand.commandFunction();
+    thiscommand->commandFunction();
 }
 
 void DietSystem::BindFunctions()
@@ -74,20 +49,55 @@ void DietSystem::BindFunction( const std::string &s, std::function<void()> f )
     dietCommands.commands[ s ].commandFunction = f;
 }
 
+void DietSystem::CheckParamCount()
+{
+    int defaultParam = thiscommand->hasDefaultParam;
+    if( ( param_tokens.size( ) - 1 ) != ( thiscommand->params.size( ) - defaultParam ) ) {
+        throw invalid_argument( "Incorrect number of params" );
+    }
+}
+
+void DietSystem::InitializeCommand( const string &name ) 
+{
+    thiscommand = &dietCommands.commands[ name ];
+    param_iter = thiscommand->params.begin();
+    tokens_iter = param_tokens.begin();
+    tokens_iter++;
+}
+
+void DietSystem::SetParam()
+{
+    ( *param_iter )->SetValue( *tokens_iter );
+    param_iter++;
+    tokens_iter++;
+}
+
+void DietSystem::SetDefaultParam()
+{
+    //run binded default function
+    thiscommand->GetLastParam()->SetDefault();
+}
+
+void DietSystem::PrintParams()
+{
+    for( auto &p : thiscommand->params ) {
+        cout << p->name << " " << p->GetValue( ) << "\n";
+    }
+}
+
 //command functions
 void DietSystem::Command_Eat()
 {
-    auto &thiscommand = dietCommands.commands[ "eat" ];
     cout << "Executing eat command:" << "\n\n";
 
-    //just print params for now
-    for( auto &p : thiscommand.params ) {
-        cout << p->name << " " << p->GetValue() << "\n";
-    }
+    SetParam();
+    SetParam();
+    SetDefaultParam();
+    PrintParams();
 
-    Name *foodName = dynamic_cast<Name*>(thiscommand.params[ 0 ].get());
-    Quantity *quantity = dynamic_cast<Quantity*>( thiscommand.params[ 1 ].get() );
-    Time *time = dynamic_cast< Time* >( thiscommand.params[ 2 ].get() );
+    Name *foodName = dynamic_cast<Name*>( thiscommand->params[ 0 ].get( ) );
+    Quantity *quantity = dynamic_cast<Quantity*>( thiscommand->params[ 1 ].get( ) );
+    Time *time = dynamic_cast< Time* >( thiscommand->params[ 2 ].get( ) );
 
     //TODO
     //find out if given food / recipe exists
--- DietTracker/DietSystem.h
+++ DietTracker/DietSystem.h
@@ -21,9 +21,32 @@ public:
 
     DietCommands dietCommands;
     DailyLog dailyLog;
+
 private:
+
     //bind A function to dietCommands
     void BindFunction( const std::string &s, std::function<void()> f );
+
+    void InitializeCommand( const string &name );
+
+    //these set params from tokens_iter (and increment iter)
+    
+    //Sets param from tokens and moves iter
+    void SetParam( );
+
+    //Sets default param from tokens and moves iter
+    void SetDefaultParam( );
+
+    void CheckParamCount( );
+
+    void PrintParams();
+    
+    //param variables
+    std::vector<unique_ptr<CommandParamBase>>::iterator param_iter;
+    std::vector<string>::iterator tokens_iter;
+    vector<string> param_tokens;
+
+    DietCommand *thiscommand;
 };
 
 #endif
